abd17a229dbc9eabc58c75de1750b49b1e9ac008
Camila Martinez <camilamarb98@gmail.com>
2024-03-12 11:21:52 +0100

    update colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,11 +4,19 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- Special settings
+function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Catppuccin Frappe"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
-
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.enable_tab_bar = false
 
 config.font = wezterm.font('Fira Code')
